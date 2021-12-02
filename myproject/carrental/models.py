f9d298b101bbe4a9459a327f8b3befb466eadbf3
Christopher Hipp Kuut <79537682+christopher-hipp@users.noreply.github.com>
2021-12-02 08:04:40 +0200

    Create website skeleton and add first model Car

--- myproject/carrental/models.py
+++ myproject/carrental/models.py
@@ -1,3 +1,31 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from uuid import uuid4
+
+
+"""class Make(models.Model):
+    make = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.make"""
+
+
+class Car(models.Model):
+    """Model representing a car, but not a specific copy of one."""
+    make = models.CharField(max_length=100)
+    body_type = models.CharField(max_length=100)
+    mileage = models.IntegerField()
+    location = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.make
+
+
+class CarInstance(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          help_text="Unique id for each car")
+    car = models.ForeignKey("Car", on_delete=models.RESTRICT, null=True)
+    due_date = models.DateField(null=True, blank=True)
+
+
